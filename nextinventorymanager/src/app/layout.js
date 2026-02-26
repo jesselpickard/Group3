e710da03e4f492d936413096ef7a504bc4b6947c
magicninja103 <134741941+magicninja103@users.noreply.github.com>
2026-02-26 11:54:58 -0600

    correcting a typo

--- nextinventorymanager/src/app/layout.js
+++ nextinventorymanager/src/app/layout.js
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "InventoryManger",
+  title: "InventoryManager",
   description: "Magic the Gathering Inventory Manager capstone project",
 };
 
