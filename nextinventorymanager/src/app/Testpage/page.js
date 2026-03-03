4e5c865feca3f5b96a1044a65e2bfe0879e33feb
magicninja103 <134741941+magicninja103@users.noreply.github.com>
2026-03-03 11:43:16 -0600

    Update page.js

--- nextinventorymanager/src/app/Testpage/page.js
+++ nextinventorymanager/src/app/Testpage/page.js
@@ -41,7 +41,7 @@ function CollapsibleMenu(){
 function CardTypeBox(){
   return(
     <select>
-      <option></option>
+      <option>Card type</option>
       <option value="artifact">Artifact</option>
       <option value="battle">Battle</option>
       <option value="creature">Creature</option>
