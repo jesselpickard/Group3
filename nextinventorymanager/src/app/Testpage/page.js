c801da28ff25dd249d29589ab8c8eba08e9f728a
magicninja103 <134741941+magicninja103@users.noreply.github.com>
2026-03-05 11:11:06 -0600

    move placement

--- nextinventorymanager/src/app/Testpage/page.js
+++ nextinventorymanager/src/app/Testpage/page.js
@@ -11,7 +11,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <CollapsibleMenu/>
-      <SubtypeSearch/>
     </div>
   );
 }
@@ -32,6 +31,7 @@ function CollapsibleMenu(){
               <input type="text" placeholder="Filter:..."></input>
             </div>
             <CardTypeBox/>
+            <SubtypeSearch/>
           </>
         )}
       </div>
