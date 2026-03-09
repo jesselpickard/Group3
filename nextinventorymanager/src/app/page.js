f8ab253cd1816e11ea5825a290e969e6d6d410a5
magicninja103 <134741941+magicninja103@users.noreply.github.com>
2026-03-09 18:23:37 -0500

    Update page.js

--- nextinventorymanager/src/app/page.js
+++ nextinventorymanager/src/app/page.js
@@ -10,14 +10,7 @@ import Menu from './components/CollapsibleMenu';
 export default function Home() {
   return (
     <div>
-      <Navbar />
-      <div className="main-layout">
-        {/* LEFT SIDEBAR */}
-        {/* MAIN CONTENT */}
-        <div className="content-area">
-          <CardGrid />
-        </div>
-      </div>
+      <CardGrid />
       <p>Link to test page [TestPage](<Link href="/Testpage">/Testpage</Link>)(<Link href="/CardInfo">/CardInfo</Link>).</p>
     </div>
   );
