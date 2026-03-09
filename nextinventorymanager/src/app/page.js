c9029b411fdf15a60e9b212abaddb073d17bcc92
magicninja103 <134741941+magicninja103@users.noreply.github.com>
2026-03-09 18:27:38 -0500

    Shift

--- nextinventorymanager/src/app/page.js
+++ nextinventorymanager/src/app/page.js
@@ -4,17 +4,15 @@ import Link from 'next/link';
 //import {useState} from 'react';
 import Navbar from './components/Navbar';
 import CardGrid from './components/CardGrid';
-import Menu from './components/CollapsibleMenu';
 
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <CardGrid />
       <p>Link to test page [TestPage](<Link href="/Testpage">/Testpage</Link>)(<Link href="/CardInfo">/CardInfo</Link>).</p>
     </div>
   );
 }
 
-//      <CardGrid />
-  //    <Menu />
