f0c9d0dcecfb6a3ba7423950945bbd72058183e6
magicninja103 <134741941+magicninja103@users.noreply.github.com>
2026-03-09 17:48:05 -0500

    utilizing Layout instead of row component

--- nextinventorymanager/src/app/page.js
+++ nextinventorymanager/src/app/page.js
@@ -5,15 +5,22 @@ import {useState} from 'react';
 import Navbar from './components/Navbar';
 import CardGrid from './components/CardGrid';
 import Menu from './components/CollapsibleMenu';
-import {Row} from './components/CollapsibleMenu';
 
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <Row itemA={<Menu/>} itemB={<CardGrid/>}/>
-      <p>Link to test page [TestPage](<Link href="/Testpage">/Testpage</Link>)(<Link href="/CardInfo">/CardInfo</Link>).</p>
+      <div className="main-layout">
+        {/* LEFT SIDEBAR */}
+        <div className="sidebar-area">
+          <CollapsibleMenu />
+        </div>
+        {/* MAIN CONTENT */}
+        <div className="content-area">
+          <CardGrid />
+        </div>
+      </div>
     </div>
   );
 }
