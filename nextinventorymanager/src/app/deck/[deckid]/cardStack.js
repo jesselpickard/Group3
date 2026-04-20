430f316b610d2d6f96d51c6e25900cd1c6231ffb
magicninja103 <134741941+magicninja103@users.noreply.github.com>
2026-04-20 11:42:47 -0500

    set up files for later

---
+++ nextinventorymanager/src/app/deck/[deckid]/cardStack.js
@@ -0,0 +1,24 @@
+
+
+/**
+ * This component will allow cards to be visually represented as a stack while in a deck page.
+ * When the user hovers their mouse over a card it should gain focus so that they can view the 
+ * whole image.
+ * 
+ * This file should also contain a function that sets up the cards image, overlaying the quantity
+ * controls over it. 
+ * 
+ * CardStack - Type is given to the item to know how to label the stack, while cards will be the contents
+ * CardImg - Takes an ID representing a card in order to associate the quantity control and source the image
+ *  from scryfall
+ */
+
+
+
+export default function CardStack(type, cards){
+
+}
+
+function CardImg(cardId){
+
+}
