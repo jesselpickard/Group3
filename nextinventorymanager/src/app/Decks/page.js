5cb2e3cae32609119b9014d8d74f7de6c9f56af0
Serendipiitea630 <kpoptrash27@gmail.com>
2026-04-21 15:34:40 -0500

    Update page.js
    attempt 2 at getting login to redirect to decks

--- nextinventorymanager/src/app/Decks/page.js
+++ nextinventorymanager/src/app/Decks/page.js
@@ -14,7 +14,7 @@ export default async function Decks() {
   } = await supabase.auth.getUser();
 
   if (authError || !user) {
-    redirect('/login?next=/Inventory');
+    redirect('/login?next=/Decks');
   }
   return (
     <div>
