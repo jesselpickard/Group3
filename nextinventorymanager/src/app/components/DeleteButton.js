8522753d1d06e97aedd8c521a55e032c80f865b2
TheSinkagu <dewaynes1203@gmail.com>
2026-04-26 22:51:00 -0500

    Copied Styling, to see how it looks

--- nextinventorymanager/src/app/components/DeleteButton.js
+++ nextinventorymanager/src/app/components/DeleteButton.js
@@ -2,7 +2,7 @@
 
 import { useRouter } from 'next/navigation';
 import { createClient } from '@/lib/supabase/client';
-
+import "./deleteStyle.css";
 //Delete button
 export default function DeleteButton({ deckId }) {
   const router = useRouter();
