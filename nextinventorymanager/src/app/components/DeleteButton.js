1d7ae55bb6a611b00706ae9ed39410ca3d1f8c70
TheSinkagu <dewaynes1203@gmail.com>
2026-04-26 22:36:58 -0500

    Delete attempt 3

--- nextinventorymanager/src/app/components/DeleteButton.js
+++ nextinventorymanager/src/app/components/DeleteButton.js
@@ -23,7 +23,7 @@ export default function DeleteButton({ deckId }) {
       return;
     }
 
-    router.push('/Decks/page');
+    router.push('@/app/Decks/page');
   }
 
   return <button onClick={deleteDeck}>Delete Deck</button>;
